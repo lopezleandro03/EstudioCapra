14de4dd1964da7140c1f3240059cc05df04cd546
lopezleandro03 <lopezleandro03@gmail.com>
2016-05-31 20:43:13 -0300

    Check-in base - Esqueleto MVC App

---
+++ EstudioCapra/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EstudioCapra.Startup))]
+namespace EstudioCapra
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
