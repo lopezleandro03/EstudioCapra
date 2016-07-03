4bdf80d8ddd82f7e6cf096641f12d5ec17e87b56
lopezleandro03 <lopezleandro03@gmail.com>
2016-07-03 16:06:31 -0300

    Add Common libraries

---
+++ Common/DbConnectionUtility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class DbConnectionUtitly
+    {
+        public static string GetConnectionString()
+        {
+            var uriString = ConfigurationManager.AppSettings["SQLSERVER_URI"];
+            var uri = new Uri(uriString);
+            var connectionString = new SqlConnectionStringBuilder
+            {
+                DataSource = uri.Host,
+                InitialCatalog = uri.AbsolutePath.Trim('/'),
+                UserID = uri.UserInfo.Split(':').First(),
+                Password = uri.UserInfo.Split(':').Last(),
+            }.ConnectionString;
+
+            return connectionString.ToString();
+        }
+
+        public static string GetConnectionStringName()
+        {
+            return DevelopmentUtility.IsDevelopmentServer() ?
+                  ConfigurationManager.AppSettings["LocalConnectionStringName"].ToString()
+                : ConfigurationManager.AppSettings["WebConnectionStringName"].ToString();
+        }
+    }
+}
